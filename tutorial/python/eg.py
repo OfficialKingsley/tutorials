04797caa860f3f21646f982228eaf6fd9ef65d92
Kingsley Ihemelandu <kingsleyihemelanduofficial@gmail.com>
2022-03-24 22:08:30 +0100

    Make changes to javascript folder
    Add more basic concepts of python
    
    Add an intro into TypeScript

--- tutorial/python/eg.py
+++ tutorial/python/eg.py
@@ -1,12 +1,15 @@
-matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
-]
-print(matrix[0][1]) #2
+import random
 
 
-#Iterating over matrix lists with nested loops
-for row in matrix:
-  for cell in row:
-    print(cell)
+class Dice():
+  def roll(self):
+    for i in range(2):
+      value1 = random.randint(1, 6)
+      value2 = random.randint(1, 6)
+      value_gotten = (value1, value2)
+    return value_gotten
+  
+
+dice = Dice()
+gotten = dice.roll()
+print(gotten)
