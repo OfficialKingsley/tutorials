02d4fe04c493b2e2e237ec84c1960dde0395b92f
Kingsley Ihemelandu <kingsleyihemelanduofficial@gmail.com>
2022-03-24 04:33:11 +0100

    Changes made to the python files and add a few extra tutorials

---
+++ tutorial/python/eg.py
@@ -0,0 +1,12 @@
+matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+print(matrix[0][1]) #2
+
+
+#Iterating over matrix lists with nested loops
+for row in matrix:
+  for cell in row:
+    print(cell)
